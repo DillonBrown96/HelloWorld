b338058b2533c9c7f66dff77f7f4b31ce699e05c
Dillon <s0299646@student.rockvalleycollege.edu>
2023-08-30 19:14:20 -0500

    Initial Commit

---
+++ HelloWorld/HelloWorldApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloWorldApp.swift
+//  HelloWorld
+//
+//  Created by Dillon Brown on 8/30/23.
+//
+
+import SwiftUI
+
+@main
+struct HelloWorldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
